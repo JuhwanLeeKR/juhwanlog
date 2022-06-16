cd1739ad8c4336238aaabd81338743233fdd6d26
Juhwan <dev.juhwanlee@gmail.com>
2022-06-16 16:06:08 +0900

    Add logo with svg

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,8 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import DarkModeToggleBtn from './darkModeToggleBtn';
+import LogoJuhwan from './assets/logoJuhwan';
+import LogoPortfolio from './assets/logoPortfolio';
 
 export default function Header() {
   return (
@@ -7,27 +10,16 @@ export default function Header() {
       <header className='text-gray-600 body-font'>
         <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
           <Link href={'/'}>
-            <a className='flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                stroke='currentColor'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                className='w-10 h-10 text-white p-2 bg-indigo-500 rounded-full'
-                viewBox='0 0 24 24'
-              >
-                <path d='M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5'></path>
-              </svg>
-              <span className='ml-3 text-xl'>이주환 portfolio</span>
+            <a className='w-60 flex gap-2'>
+              <LogoJuhwan />
+              <LogoPortfolio />
             </a>
           </Link>
           <nav className='md:ml-auto flex flex-wrap items-center text-base justify-center'>
             <Link href={'/'}>
               <a className='mr-5 hover:text-gray-900'>HOME</a>
             </Link>
-            <Link href={'skills'}>
+            <Link href={'/skills'}>
               <a className='mr-5 hover:text-gray-900'>Skills</a>
             </Link>
             <Link href={'/projects'}>
