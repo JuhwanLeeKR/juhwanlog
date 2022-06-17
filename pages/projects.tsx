3bc507b15ecb1ebf1d4c5bc2c06d0a36264506fb
Juhwan <dev.juhwanlee@gmail.com>
2022-06-17 19:40:01 +0900

    Fetch data with notion API

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,7 +1,10 @@
 import Layout from '../components/layout';
 import Head from 'next/head';
+import { TOKEN, DATABASE_ID } from '../config';
 
-export default function Projects() {
+export default function Projects({ projects }: any) {
+  //console.log(projects);
+
   return (
     <Layout>
       <Head>
@@ -12,7 +15,52 @@ export default function Projects() {
         />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <h1>프로젝트</h1>
+      <h1>{projects.results.length}개의 프로젝트가 있습니다.</h1>
+
+      {projects.results.map((aProject: any) => (
+        <h1 key={aProject.properties.Name.title[0].plain_text}>
+          {aProject.properties.Name.title[0].plain_text}
+        </h1>
+      ))}
     </Layout>
   );
+}
+
+// 빌드 타임에 호출한다
+export async function getStaticProps() {
+  const options = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Notion-Version': '2022-02-22',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({
+      sorts: [
+        {
+          property: 'WorkPeriod',
+          direction: 'descending',
+        },
+      ],
+      page_size: 100,
+    }),
+  };
+
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    options
+  );
+
+  const projects = await res.json();
+
+  // const projectNames = projects.results.map(
+  //   (aProjects: any) => aProjects.properties.Name.title[0].plain_text
+  // );
+  //console.log(`projectNames: ${projectNames}`);
+  //console.log(projects);
+
+  return {
+    props: { projects },
+  };
 }
